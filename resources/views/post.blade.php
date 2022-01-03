ab5590c015811a968faf17831d501dd9bb04f0b1
ayatullah-ayat <ayat2486@gamil.com>
2022-01-03 16:17:54 +0600

    make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<body>
+    
+    <article>
+        <h1>My First Post</h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis consectetur unde exercitationem placeat doloremque, porro, culpa adipisci vel itaque ipsum ipsa animi sit non temporibus maxime. Veniam, ea! Deleniti soluta libero consequatur reprehenderit dolorum! Praesentium asperiores quod unde corrupti nulla animi tempora cupiditate, vitae optio nesciunt, natus consequatur perferendis provident nemo magni laudantium! Perferendis natus cum modi a, placeat quos fugit? Hic repellat id delectus minima, eligendi ea doloribus quo animi facere laborum obcaecati iusto ut vero ducimus temporibus expedita.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+
+    </head>
+    <body>
+
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere vitae dignissimos rem nesciunt corporis eum consequatur debitis velit reprehenderit aut sapiente. Officia odio, illo vero ab dicta ducimus veritatis.</p>
+        </article>
+    
+        <article>
+            <h1><a href="/post">My Second Post</a></h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere vitae dignissimos rem nesciunt corporis eum consequatur debitis velit reprehenderit aut sapiente. Officia odio, illo vero ab dicta ducimus veritatis.</p>
+        </article>
+
+        <article>
+            <h1><a href="/post">My Third Post</a></h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere vitae dignissimos rem nesciunt corporis eum consequatur debitis velit reprehenderit aut sapiente. Officia odio, illo vero ab dicta ducimus veritatis.</p>
+        </article>
+    
+    </body>
+</html>
