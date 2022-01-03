5438fbe7d1e1c1aee32be914a5d79caa42c8a9d3
ayatullah-ayat <ayat2486@gamil.com>
2022-01-03 16:46:44 +0600

    store blog post as html files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,7 @@
 <body>
     
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis consectetur unde exercitationem placeat doloremque, porro, culpa adipisci vel itaque ipsum ipsa animi sit non temporibus maxime. Veniam, ea! Deleniti soluta libero consequatur reprehenderit dolorum! Praesentium asperiores quod unde corrupti nulla animi tempora cupiditate, vitae optio nesciunt, natus consequatur perferendis provident nemo magni laudantium! Perferendis natus cum modi a, placeat quos fugit? Hic repellat id delectus minima, eligendi ea doloribus quo animi facere laborum obcaecati iusto ut vero ducimus temporibus expedita.
-        </p>
+        <?= $post ?>
     </article>
 
     <a href="/">Go Back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,19 +9,19 @@
     <body>
 
         <article>
-            <h1><a href="/post">My First Post</a></h1>
+            <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere vitae dignissimos rem nesciunt corporis eum consequatur debitis velit reprehenderit aut sapiente. Officia odio, illo vero ab dicta ducimus veritatis.</p>
         </article>
     
         <article>
-            <h1><a href="/post">My Second Post</a></h1>
+            <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere vitae dignissimos rem nesciunt corporis eum consequatur debitis velit reprehenderit aut sapiente. Officia odio, illo vero ab dicta ducimus veritatis.</p>
         </article>
 
         <article>
-            <h1><a href="/post">My Third Post</a></h1>
+            <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere vitae dignissimos rem nesciunt corporis eum consequatur debitis velit reprehenderit aut sapiente. Officia odio, illo vero ab dicta ducimus veritatis.</p>
         </article>
